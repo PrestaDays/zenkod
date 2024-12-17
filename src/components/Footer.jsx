8f45e54f2cfc0324438657e433e737efc63a80ff
x7pot2 <bertrandguillaume524@gmail.com>
2024-12-17 14:43:28 +0100

    Mise en place du footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,20 @@
+import "../styles/Footer.css";
+
 const Footer = () => {
     return (
-        <footer>
-            <p>© 2021 All rights reserved.</p>
+        <footer class="footer">
+            <div class="footer-content">
+                <span>Presta Days</span>
+                <span>Services</span>
+                <span>Entreprise</span>
+                <span>Réseaux sociaux</span>
+            </div>
+            <div class="footer-info">
+                <span>prestadays@gmail.com</span>
+                <span>Lorem Ipsum</span>
+                <span>Qui sommes-nous ? Carrière Contact</span>
+                <span>Twitter LinkedIn Instagram GitHub</span>
+            </div>
         </footer>
     );
 }
