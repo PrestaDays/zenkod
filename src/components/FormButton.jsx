5a53a463e1d12096648a67ecfd954b3198bc75c3
theoMANEVIT31 <theo.manevit@ynov.com>
2024-12-17 15:39:38 +0100

    page contact

---
+++ src/components/FormButton.jsx
@@ -0,0 +1,9 @@
+const Button = ({ label }) => {
+  return (
+    <button className="px-4 py-2 bg-[#D57A66] text-white rounded hover:bg-[#C05050]">
+      {label}
+    </button>
+  );
+};
+
+export default Button;
