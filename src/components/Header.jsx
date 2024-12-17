df9ccfca0c9bd42ee9130675d033058cb242d1b8
x7pot2 <bertrandguillaume524@gmail.com>
2024-12-17 14:11:56 +0100

    Ajout de l'import du Header menu dans la page d'acceuil

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
-import ThemeToggle from "./ThemeToggle.jsx";
-
 const Header = () => {
     return (
         <header>
-            <ThemeToggle />
+            <div class="nav">
+                <img src="https://i.imgur.com/1tMFzp8.png" alt="Logo" class="logo" />
+                <span class="nav-item">Services</span>
+                <span class="nav-item">À propos</span>
+                <div class="contact-button">
+                    <span>Contact</span>
+                </div>
+            </div>
         </header>
     );
 }
