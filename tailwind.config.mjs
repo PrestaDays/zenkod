bc0843f947a6e018af0722f68fa47d6318265df0
Sarouman54 <sarahbarrabe@gmail.com>
2024-12-18 09:20:21 +0100

    init services page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,14 @@ export default {
 	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#F2E8CF",
+				secondary: "#2A4747",
+				tertiary: "#4A6670",
+				accent: "#D57A66"
+			}
+		},
 	},
 	plugins: [
 		function ({ addComponents, theme }) {
