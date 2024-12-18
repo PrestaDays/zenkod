db393b84e21f429e048e43e82c0a07d9108b6ddb
tonycava <anthony.cavagne@ynov.com>
2024-12-18 13:29:30 +0100

    add: fonts vesper / rgpd page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,9 +3,20 @@ export default {
 	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		container: {
+			center: true,
+		},
+		extend: {
+			fontFamily: {
+				'vesper-black': ['vesper-black'],
+				'vesper-bold': ['vesper-bold'],
+				'vesper-medium': ['vesper-medium'],
+				'vesper-regular': ['vesper-regular'],
+			}
+		},
 	},
 	plugins: [
+		require('@tailwindcss/typography'),
 		function ({ addComponents, theme }) {
 			addComponents({
 				'.btn': {
