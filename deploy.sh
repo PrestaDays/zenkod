cd4e4d06befc5b09c5bf4eb82fed128f918e9b59
lucas <lucas.escaffre@ynov.com>
2025-11-19 10:16:01 +0100

    refactor: clean up comments and improve header styling

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
+# Nettoie AVANT de pull pour libérer de l'espace
+docker system prune -a -f --volumes
+
+# Pull les nouvelles images
 docker-compose pull
 
+# Redémarre les services
 docker-compose up -d
 
+# Nettoie APRÈS pour supprimer les anciennes versions
 sleep 10
-
-docker image prune -f
+docker system prune -a -f --volumes
