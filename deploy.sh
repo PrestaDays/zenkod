d1a77b01a173d8d2d9d18e8ba4f7ad97fdfec7d3
lucas <lucas.escaffre@ynov.com>
2025-11-19 14:39:38 +0100

    fix: deploy.sh avec docker-compose down pour éviter conflits de containers

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# Arrêter et supprimer tous les containers
+docker-compose down --remove-orphans
+
+# Pull les nouvelles images
 docker-compose pull
 
-docker-compose up -d --force-recreate --remove-orphans
+# Redémarrer avec les nouvelles images
+docker-compose up -d
 
+# Nettoyer les anciennes images et volumes après 10s
 sleep 10
 docker image prune -a -f
 docker volume prune -f
