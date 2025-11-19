0d13687127a095bda99825a5a45a1e7709430953
lucas <lucas.escaffre@ynov.com>
2025-11-19 11:22:27 +0100

    refactor: streamline deployment script by optimizing cleanup commands

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# Nettoie AVANT de pull pour libérer de l'espace
-docker system prune -a -f --volumes
-
-# Pull les nouvelles images
 docker-compose pull
 
-# Redémarre les services
 docker-compose up -d
 
-# Nettoie APRÈS pour supprimer les anciennes versions
 sleep 10
-docker system prune -a -f --volumes
+docker image prune -a -f
+docker volume prune -f
